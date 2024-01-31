68a5bbb7c1766c7a0c0661c98cf5980cd4386645
badal <badal@badals-MacBook-Air.local>
2024-01-31 22:03:42 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">hello</h1>
+    </div>
+  );
+}
+
+export default App;
