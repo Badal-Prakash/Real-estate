d51ea672696192d3c8b0b811b29cd77b23cbf47d
badal <badal@badals-MacBook-Air.local>
2024-02-01 20:56:15 +0530

    api route

--- api/index.js
+++ api/index.js
@@ -2,13 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 // import dotenv from "dotenv";
 // dotenv.config();
-mongoose.connect("mongodb+srv://lav14251:Lavsharma1@cluster0.hsgurzr.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-  console.log('connected to mongodb')
-}).then(()=>{
-  console.log("error")
-});
+import userRouter from "../api/routes/user.routes.js";
+mongoose
+  .connect(
+    "mongodb+srv://lav14251:Lavsharma1@cluster0.hsgurzr.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("error");
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000ls");
+  console.log("server is running on port 3000");
 });
+app.use("/api/user", userRouter);
