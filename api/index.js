6e78b3ce3eb8c041696bc4096f557f69f4fa67cc
badal <badal@badals-MacBook-Air.local>
2024-02-01 11:24:45 +0530

    database added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+mongoose.connect("mongodb+srv://lav14251:Lavsharma1@cluster0.hsgurzr.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+  console.log('connected to mongodb')
+}).then(()=>{
+  console.log("error")
+});
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+  console.log("server is running on port 3000ls");
 });
